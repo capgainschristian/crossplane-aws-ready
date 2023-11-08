af167821addd9340acff81d19585ec395e83d5d9
Christian Graham <capgainschristian@gmail.com>
2023-11-08 07:17:01 -0500

    fixed setup

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 # Setup ArgoCD
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm dep update charts/argo-cd/
-helm install argo-cd --namespace argo-cd charts/argo-cd/ --wait
+helm install argo-cd charts/argo-cd/ --wait
 #kubectl port-forward svc/argo-cd-argocd-server 8080:443
 kubectl get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
