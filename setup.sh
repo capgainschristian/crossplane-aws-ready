3ab6788387b5774d23fab51a71b3e5226b7a5381
capgainschristian <graham_christian@bah.com>
2023-12-08 08:39:52 -0500

    fixing argocd

--- setup.sh
+++ setup.sh
@@ -23,7 +23,12 @@ stringData:
 EOF
 rm env_vars.txt
 
-kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -n argocd
+helm repo add argo-cd https://argoproj.github.io/argo-helm
+helm dep update charts/argo-cd/
+helm install argo-cd charts/argo-cd/ --namespace argocd
+kubectl delete secret -l owner=helm,name=argo-cd
+
+#kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -n argocd
 #kubectl wait --for=condition=ready pod $(kubectl get pods -n argocd | awk '{if ($1 ~ "argocd-server-") print $1}') -n argocd --timeout=30s
 
 # Setup Crossplane
