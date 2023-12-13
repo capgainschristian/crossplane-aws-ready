5909cdafce9c78df876f29794efe6675c3dc6336
Christian Graham <63564473+capgainschristian@users.noreply.github.com>
2023-12-13 08:47:31 -0500

    Removing comments

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Start minikube
 minikube start --cpus=4 --memory=16GB
 
-# Add argocd secret
 kubectl create ns argocd
+
+# There is a cleaner way of doing this by either using kustomize or Helm
 sops -d charts/credentials/argocd-repo-creds.txt > env_vars.txt && source env_vars.txt
 cat <<EOF | kubectl apply -f -
 apiVersion: v1
@@ -29,29 +29,21 @@ helm dep update charts/argo-cd/
 helm install argo-cd charts/argo-cd/ --namespace argocd
 kubectl delete secret -l owner=helm,name=argo-cd -A
 
-#kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -n argocd
-#kubectl wait --for=condition=ready pod $(kubectl get pods -n argocd | awk '{if ($1 ~ "argocd-server-") print $1}') -n argocd --timeout=30s
-
-# Setup Crossplane
 kubectl create namespace crossplane-system
 helm repo add crossplane-stable https://charts.crossplane.io/stable
 helm repo update
 helm install crossplane --namespace crossplane-system crossplane-stable/crossplane --wait
 
-# Create AWS creds secret for crossplane
 sops -d crossplane/aws/credentials/aws-credentials.txt > delete_me.txt
 kubectl create secret generic aws-secret -n crossplane-system --from-file=creds=./delete_me.txt
 rm delete_me.txt
 
-# Generate AWS providers
 kubectl apply -f crossplane/aws/aws.yaml
 kubectl wait provider/provider-aws --for=condition=Healthy --timeout=240s
 kubectl apply -f crossplane/aws/provider_config.yaml
 
-# App of Apps install
 helm template charts/apps/ | kubectl apply -f -
 
-# ArgoCD password
 echo "To login to argoCD web GUI, please use the following password.."
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d
 #kubectl port-forward svc/argocd-server 8080:443 -n argocd
